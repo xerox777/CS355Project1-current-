a306309eca3f599c0efe3ccf936546123d0fd131
grant <aliengurujesus@gmail.com>
2018-04-15 11:02:12 -0700

    uptodate

--- dal/products_dal.js
+++ dal/products_dal.js
@@ -12,7 +12,8 @@ exports.getAll = function(callback) {
 };
 
 exports.insert = function(params, callback) {
-    var query = 'INSERT INTO products (computer, tablet, phone, other, ecorp_id) VALUES (?,?,?,?,?)';
+    //var query = 'INSERT INTO products (computer, tablet, phone, other, ecorp_id) VALUES (?,?,?,?,?)';
+    var query = 'call insert_product(?, ?, ?, ?, ?)';
     var queryData = [params.computer,  params.tablet, params.phone, params.other, params.ecorp_id];
 
     connection.query(query, queryData, function(err, result) {
